d34a4f8957d75a28f4262a52a0b57d7223440309
Miguel-Escobar <77211681+Miguel-Escobar@users.noreply.github.com>
2022-08-23 23:50:44 -0400

    Update P2.py

--- Tarea 2/P2.py
+++ Tarea 2/P2.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.special import gammainc, gamma
 x = np.array([-0.15, -0.1, -0.05, 0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1])
 
 F = np.array([[np.sin(-0.15*2*np.pi), np.sin(-0.1*2*np.pi), np.sin(-0.05*2*np.pi), np.sin(0*2*np.pi), np.sin(0.1*2*np.pi), np.sin(0.2*2*np.pi), np.sin(0.3*2*np.pi), np.sin(0.4*2*np.pi), np.sin(0.6*2*np.pi), np.sin(0.7*2*np.pi), np.sin(0.8*2*np.pi), np.sin(0.9*2*np.pi), np.sin(1*2*np.pi)],
@@ -8,7 +8,9 @@ F = np.array([[np.sin(-0.15*2*np.pi), np.sin(-0.1*2*np.pi), np.sin(-0.05*2*np.pi
 
 y = np.array([180, 150, 80, 0, -110, -180, -182, -120, 120, 190, 198, 130, 0]).transpose()
 
-sigma = np.diag([5,6,7,2,3,10,11,10,5,6,3,7,11])
+sigmalista = np.array([5,6,7,2,3,10,11,10,5,6,3,7,11])**2
+
+sigma = np.diag(sigmalista)
 
 sinv = np.linalg.inv(sigma)
 
@@ -18,11 +20,20 @@ alphainv = np.linalg.inv(F.transpose() @ sinv @ F)
 
 b = (F.transpose() @ sinv @ y)
 
-parametros = alphainv @ b #np.linalg.inv(F.transpose() @ sinv @ F) @ (F.transpose() @ sinv @ y)
+parametros = alphainv @ b 
 
 A = parametros[0]
 B = parametros[1]
+
+def ajuste(t):
+    return A*np.sin(2*np.pi*t) + B*np.cos(2*np.pi*x)
+
+chi2 = np.sum(((y-ajuste(x))**2)/sigmalista)
+chindof = chi2/11
+
+p = gammainc(11/2, chi2/2)/gamma(11/2)
+
 plt.figure(1)
-plt.plot(x, A*np.sin(2*np.pi*x) + B*np.cos(2*np.pi*x))
+plt.plot(x, ajuste(x))
 plt.plot(x, y)
 
