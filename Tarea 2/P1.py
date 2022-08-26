22b6e95ace51d60831f2b674356d7cf76d0d4c9a
Miguel-Escobar <miguelescobar@ug.uchile.cl>
2022-08-25 22:42:09 -0400

    wawawa

--- Tarea 2/P1.py
+++ Tarea 2/P1.py
@@ -18,6 +18,7 @@ initial = (3 - s + np.sqrt((s-3)**2 + 24*s))/(12*s) # aproximación de la raíz 
 k = sp.root_scalar(optimizable, x0=initial, fprime=derivada, method='newton')
 k = k.root
 b = 20*k/np.sum(t)
+
 print(k, b)
 
 plt.figure()
--- Tarea 2/P2.py
+++ Tarea 2/P2.py
@@ -8,9 +8,9 @@ F = np.array([[np.sin(-0.15*2*np.pi), np.sin(-0.1*2*np.pi), np.sin(-0.05*2*np.pi
 
 y = np.array([180, 150, 80, 0, -110, -180, -182, -120, 120, 190, 198, 130, 0]).transpose()
 
-sigmalista = np.array([5,6,7,2,3,10,11,10,5,6,3,7,11])**2
+sigmalista = np.array([5,6,7,2,3,10,11,10,5,6,3,7,11])
 
-sigma = np.diag(sigmalista)
+sigma = np.diag(sigmalista**2)
 
 sinv = np.linalg.inv(sigma)
 
@@ -28,12 +28,16 @@ B = parametros[1]
 def ajuste(t):
     return A*np.sin(2*np.pi*t) + B*np.cos(2*np.pi*x)
 
-chi2 = np.sum(((y-ajuste(x))**2)/sigmalista)
+chi2 = np.sum(((y-ajuste(x))**2)/sigmalista**2)
 chindof = chi2/11
 
 p = gammainc(11/2, chi2/2)/gamma(11/2)
 
 plt.figure(1)
-plt.plot(x, ajuste(x))
-plt.plot(x, y)
-
+plt.clf()
+plt.plot(x, ajuste(x), label="Ajuste A = " + str(np.round(A, decimals=2)) + ', B = ' + str(np.round(B, decimals=2)))
+plt.errorbar(x, y, sigmalista, fmt='o', ms=4, label='Datos')
+plt.xlabel('Tiempo normalizado')
+plt.ylabel('Velocidad Radial [km/s]')
+plt.legend()
+plt.show()
