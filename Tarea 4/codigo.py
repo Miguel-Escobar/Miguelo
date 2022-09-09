75639ccac75b23f56f0ba1f676421b57f9f84020
Miguel-Escobar <77211681+Miguel-Escobar@users.noreply.github.com>
2022-09-09 12:47:02 -0300

    funciona!!!!!!!!

--- Tarea 4/codigo.py
+++ Tarea 4/codigo.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 
 def ll(E, k, theta):
     primer_término = np.log(2*np.pi*np.sqrt(E)) + loggamma(k + 1)
-    segundo_término = -(np.log(np.pi**(3/2) * theta**(3/2)) + (1/3)*np.log(k) + loggamma(k - 1/2))
+    segundo_término = -(np.log(np.pi**(3/2) * theta**(3/2)) + (3/2)*np.log(k) + loggamma(k - 1/2))
     tercer_término = -(k + 1) * np.log(1 + E/(k*theta))
     return np.sum(primer_término + segundo_término + tercer_término)
+
 
 # Importar datos:
 
@@ -18,13 +19,14 @@ data = np.genfromtxt("energias_electrones.csv")
 
 def paso_metropolis(a, datos):
     acandidato=np.zeros(2)
-    acandidato[0]=a[0]+np.random.normal(scale=0.1)
-    acandidato[1]=a[1]+np.random.normal(scale=0.1)
+    acandidato[0]=a[0] + np.random.normal(scale=1)
+    acandidato[1]=a[1] + np.random.normal(scale=1)
     if acandidato[0] <= 0:
         acandidato[0] = a[0]
     if acandidato[1]<=0:
         acandidato[1] = a[1]
-    logr = ll(datos,*acandidato) - ll(datos,*a)
+
+    logr = ll(datos,acandidato[0], acandidato[1]) - ll(datos,a[0], a[1])
 
     if np.log(np.random.uniform(0,1))<logr:
         return acandidato
@@ -32,25 +34,23 @@ def paso_metropolis(a, datos):
         return a
 
 def METROPOLIS(ndatos, datos, temper, a0):
-    temperable = np.zeros((temper, 2))
-    temperable[0] = a0
-    for i in range(temper-1):
-        temperable[i+1] = paso_metropolis(temperable[i], datos)
-    resultados = np.zeros((ndatos, 2))
-    resultados[0] = temperable[temper-1]
-    for i in range(ndatos-1):
-        resultados[i+1] = paso_metropolis(resultados[i], datos)
-    
-    return resultados
+    resultados = []
+    params = a0
+    for i in range(temper):
+        params = paso_metropolis(params, datos)
+    for i in range(ndatos):
+        params = paso_metropolis(params, datos)
+        resultados.append(params)
+    return np.array(resultados)
 
 def analiza(seriea):
     print("Promedios:",np.mean(seriea,axis=0))
     plt.figure()
     plt.clf()
-    plt.hist2d(seriea[:,0],seriea[:,1],100)
+    plt.hist2d(seriea[:,0],seriea[:,1], 100)
     plt.show()
 
-result = METROPOLIS(1000, data, 20000, [10, 10])
+result = METROPOLIS(100000, data, 2000, [3, 1])
 
 analiza(result)
  
