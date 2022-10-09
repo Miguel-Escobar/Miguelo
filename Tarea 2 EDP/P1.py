dfe66fb6a2437a1a80340744958d6a710da3c55b
Miguel-Escobar <miguelescobar@ug.uchile.cl>
2022-10-08 21:09:14 -0300

    Implementado SOR

--- Tarea 2 EDP/P1.py
+++ Tarea 2 EDP/P1.py
@@ -1,32 +1,83 @@
 import numpy as np
-from scipy.linalg import solve_banded
-N = 300
+import matplotlib.pyplot as plt
+from tqdm import trange
+
+N = 100
 L = 1
-R = L/4
+r = L/4
 V0 = 1
 D = 100
 dx = L/N
+w = 2/(1 + np.pi/N)
 
 # Array con distancias desde el centro:
 
 distancia_al_centro = np.zeros((N, N))
-medio = int(N/2)
+medio = int(N/2)-1
 for i in range(N):
     for j in range(N):
-        distancia_al_centro[i, j] = np.sqrt((medio - i)**2 + (medio - j)**2)*dx 
-
+        distancia_al_centro[i, j] = np.sqrt((medio - i + 1/2)**2 + (medio - j + 1/2)**2)*dx 
 
 # Setear condiciones de borde:
 
-def bordes(array, radius):
-    copy = np.copy(array)
-    copy[distancia_al_centro > radius] = -V0
-    copy[0,:] = V0
-    copy[:,0] = V0
-    copy[-1,:] = V0
-    copy[:,-1] = V0
-    
+def bordes(array):
+    array[distancia_al_centro < r] = -V0
+    array[0,:] = V0
+    array[:,0] = V0
+    array[-1,:] = V0
+    array[:,-1] = V0
+    return
 
-    
+# test región interior:
+
+def region_interior(i, j):
+    return i < N-1 and i > 0 and j < N-1 and j > 0 and distancia_al_centro[i, j] > r
+
+# iteración Gauss-Seidel:
+
+def gs(array):
+    length, width = array.shape
+    for i in range(length):
+        for j in range(width):
+            if region_interior(i, j):
+                array[i, j] = (array[i, j - 1] + array[i, j + 1] + array[i - 1, j] + array[i + 1, j])/4
+    return
+
+# sobrerelajación:
+
+def sobrerelax(array):
+    length, width = array.shape
+    for i in range(length):
+        for j in range(width):
+            if region_interior(i, j):
+                array[i, j] = (1-w)*array[i, j] + (array[i, j - 1] + array[i, j + 1] + array[i - 1, j] + array[i + 1, j])*w/4 
+    return
+
+# test de convergencia (cortesía de prof. Valentino en curso de met num para la ciencia):
+
+def convergio(phi, phi_anterior, rtol=0.1):
+    not_zero = phi_anterior != 0
+    dif_relativa = ((phi_anterior[not_zero] - phi[not_zero]) / phi_anterior[not_zero])
+    return np.fabs(dif_relativa).max() < rtol
+
+
+sol = np.zeros((N,N))
+bordes(sol)
+solcopy = sol.copy()
+counter = 0
+maxiter = 10000
+
+while True:
+    solcopy = sol.copy()
+    sobrerelax(sol)
+    counter += 1
+    if (counter > 1 and convergio(sol, solcopy)) or counter > maxiter:
+        break
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.pcolormesh(sol)
+ax.set_xlabel("SOR")
+fig.show()
 
 
