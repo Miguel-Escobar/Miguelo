5aad01b702c4bffef06c343285c40e5260814714
Miguel-Escobar <77211681+Miguel-Escobar@users.noreply.github.com>
2022-12-19 15:19:53 -0300

    wa funciona p1 wiiii

--- Tarea Helio/variationalmontecarlo.py
+++ Tarea Helio/variationalmontecarlo.py
@@ -18,6 +18,7 @@ def pdf(R, alpha):
 
 def integral(alpha):
      #value, _ = nquad(integrable, [[-limit,limit],[-limit,limit],[-limit,limit],[-limit,limit],[-limit,limit],[-limit,limit]], args=[alpha], opts={"epsabs":10e1, "epsrel":10e1}) # Relajé mucho la tolerancia y aun así no corre
+    # Al final no normalizo, no se bien por qué.
     value = 1
     return value
 
@@ -26,13 +27,12 @@ def integrable(r1x,r1y,r1z,r2x,r2y,r2z, alpha):
     r2 = np.sqrt(r1x**2 + r1y**2 + r1z**2)
     r12 = np.sqrt((r1x-r2x)**2+(r1y-r2y)**2+(r1z-r2z)**2)
     returnable = np.exp(-2*r1 - 2*r2 + .5*(r12/(1+alpha*r12)))
-    # print("se llamó la funcion a integrar")
     return returnable
 
 # Defino el paso metropolis: 
 
 def paso_metropolis(R, alpha):    
-    newR = R + np.random.normal(loc=0.0, scale=0.33, size=R.shape)
+    newR = R + np.random.normal(loc=0.0, scale=0.4, size=R.shape)
     logr = logpdf(newR, alpha) - logpdf(R, alpha)
     accept = np.log(np.random.uniform(0,1))<logr
     if accept:
@@ -73,18 +73,17 @@ def energiaslocales(alpha, sample):
     normalizante = integral(alpha)
     estimacion = np.zeros(len(sample))
     for i in range(len(sample)):
-        estimacion[i] = energialocal(sample[i], alpha)#/normalizante*pdf(sample[i], alpha)
+        estimacion[i] = energialocal(sample[i], alpha)
     return estimacion
 
 def derlog(sample, alpha):
     r1 = np.linalg.norm(sample[:,0], axis=1)
     r2 = np.linalg.norm(sample[:,1], axis=1)
     r12 = np.linalg.norm(sample[:,0] - sample[:,1], axis=1)
-    return -2*r1 - 2*r2 - .5*(r12*(1/(1+alpha*r12)**2)*r12)#.5*(r12/(1+alpha*r12))
-# AHORA TESTEO PARA VER SI CALCULA ENERGIAS VARIACIONALES:
+    return -2*r1 - 2*r2 - .5*(r12*(1/(1+alpha*r12)**2)*r12)
 
-Ndatos = 10000
-temper = 1000
+Ndatos = 100000
+temper = 10000
 R0 = np.array([[1, 1.1, .9],[-1, -1.1, -.9]]) # Inicial al azar
 limit = np.inf # La integral se lleva a cabo en (-limit,limit)^6 (si es np.inf es lo mismo que integrar en R^6)
 Nalphas = 40
@@ -93,6 +92,7 @@ EV = np.zeros(Nalphas)
 Ratios = np.zeros(Nalphas)
 meanderlog = np.zeros(Nalphas)
 meanderlogel = np.zeros(Nalphas)
+sigma2 = np.zeros(Nalphas)
 
 for i in trange(Nalphas):
     sample, ratio = METROPOLIS(Ndatos, temper, R0, alphas[i], returnaccepted=True)
@@ -101,18 +101,39 @@ for i in trange(Nalphas):
         probabilities[j] = pdf(sample[j], alphas[i])
     el = energiaslocales(alphas[i], sample)
     dlog = derlog(sample, alphas[i])
-    EV[i] = np.mean(el)#*probabilities)
-    meanderlog[i] = np.mean(dlog)#*probabilities)
-    meanderlogel[i] = np.mean(dlog*el)#*probabilities)
+    EV[i] = np.mean(el)
+    meanderlog[i] = np.mean(dlog)
+    meanderlogel[i] = np.mean(dlog*el)
     Ratios[i] = ratio
-    
-    
+    sigma2[i] = np.mean((el - EV[i])**2)
+
 derivative = np.gradient(EV, deltaalpha)
 smoothderivative = 2*(meanderlogel-meanderlog*EV)
-plt.clf()
-plt.plot(smoothderivative, label="wea")
-plt.plot(derivative)
-plt.legend()
-plt.show()
 
-# PROBLEMA RESUELTO!!!!!!!!! GOD!!!!!!!!!!!
+fig = plt.figure(figsize=(12,5))
+fig.clf()
+axEV = fig.add_subplot(141)
+axDer = fig.add_subplot(142)
+axSigma = fig.add_subplot(143)
+axRatio = fig.add_subplot(144)
+
+axEV.plot(alphas, EV, label="Energía Variacional")
+axEV.set_xlabel(r"$\alpha$")
+axEV.set_ylabel("Energía Variacional")
+
+axDer.plot(alphas, smoothderivative, label="Derivada suave")
+axDer.plot(alphas, derivative, label="Derivada normal")
+axDer.set_xlabel(r"$\alpha$")
+axDer.set_ylabel(r"$\frac{dE_v}{d\alpha}$")
+axDer.legend()
+
+axSigma.plot(alphas, sigma2, label="Varianza")
+axSigma.set_xlabel(r"$\alpha$")
+axSigma.set_ylabel("$\sigma^2$")
+
+axRatio.plot(alphas, Ratios, label="Fracción aceptada")
+axRatio.set_xlabel(r"$\alpha$")
+axRatio.set_ylabel("Fracción aceptada")
+
+fig.tight_layout()
+fig.show()
